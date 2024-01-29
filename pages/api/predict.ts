92c8a34d3faec0afd06efd8f40138ccb3137787c
Aditya Wiguna <adityawiguna0@gmail.com>
2024-01-29 16:03:02 +0800

    implement env

--- pages/api/predict.ts
+++ pages/api/predict.ts
@@ -8,16 +8,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-    if (req.method === 'POST') {
-      const response = await fetch("https://api-inference.huggingface.co/models/Hello-SimpleAI/chatgpt-detector-roberta",
-        {
-          headers: { Authorization: "Bearer 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" },
-          method: "POST",
-          body: JSON.stringify(req.body.text),
-        }
-      );
+  if (req.method === 'POST') {
+    const response = await fetch("https://api-inference.huggingface.co/models/Hello-SimpleAI/chatgpt-detector-roberta",
+      {
+        headers: { Authorization: `Bearer ${process.env.HUGGING_FACE_API_KEY}` },
+        method: "POST",
+        body: JSON.stringify(req.body.text),
+      }
+    );
 
-      const result = await response.json();
-      res.status(200).json({ data: result[0] })
-    }
+    const result = await response.json();
+    res.status(200).json({ data: result[0] })
+  }
 }
